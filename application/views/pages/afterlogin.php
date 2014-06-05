b7a45f5b524c3d0f5c2acc0eb0d679262e650ec2
Siemen Kraayenbrink <siemenkraayenbrink@Siemens-MacBook-Pro.local>
2014-06-05 15:56:30 +0200

    redirecting new users after first login to the personality test

--- application/views/pages/afterlogin.php
+++ application/views/pages/afterlogin.php
@@ -6,10 +6,22 @@
     }
 ?>
 
-<h3> You've successfully logged in. Stand by for redirect to personalized homepage!</h3>
+
 <h2> Welcome <?php echo $user['name']; ?>!</h2>
 <a href="<?php echo base_url(); ?>index.php/login/logout">Logout</a>
 <?php
-	//we set a timer to wait for 6 seconds and then redirect the user to the homepage.
-	$this->output->set_header('refresh:6;url='.base_url());  
+	// Check if the user has already done the personality test.
+	if (strlen($user["personality"]) < 2) {
+		//Redirect user to personality test
+		echo " <h3>You've logged in and will be directed to the personality test soon!</h3>
+			<p> skip the wait by clicking <a href='".base_url()."index.php/edit/personality'>here</a>!</p>";
+		$this->output->set_header('refresh:6;url='.base_url().'index.php/edit/personality');
+	} else {
+		//we set a timer to wait for 6 seconds and then redirect the user to the homepage.
+		echo "
+			<h3> You've successfully logged in. Stand by for redirect to personalized homepage!</h3>
+		";
+		$this->output->set_header('refresh:6;url='.base_url());
+	}
+	
 ?>
