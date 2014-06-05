01cd314b1fbcebaa6b3744962edd851cc9d85518
Siemen Kraayenbrink <siemenkraayenbrink@Siemens-MacBook-Pro.local>
2014-06-05 22:29:55 +0200

    profile blocks are now shown in searchresults

--- application/views/pages/searchresults.php
+++ application/views/pages/searchresults.php
@@ -1,7 +1,36 @@
 <h1>matches (views/pages/searchresult.php)</h1>
 <?php
-	foreach($matches as $match) {
+	foreach($matches as $profile) {
 		//display the matches
-		echo $match['name'] . ' '. $match['finalScore']. "<br>";
+		//echo $match['name'] . ' '. $match['finalScore']. "<br>";
+
+        if($loggedin && $profile['profile_pic'] != "")
+        {
+            $imgString = "<img class=\"homepageSilhouette\" src=\"" . base_url() . "/uploads/" . $profile['profile_pic'] . "\" alt=\"Silhouette\">";
+        }
+        else
+        {
+            $imgString = "<img class=\"homepageSilhouette\" src=\"" . base_url() . "/includes/img/silhouetteMan.png\" alt=\"Silhouette\">";
+            if($profile['gender'] == 1)
+                $imgString = "<img class=\"homepageSilhouette\" src=\"" . base_url() . "/includes/img/silhouetteWoman.png\" alt=\"Silhouette\">";
+        }    
+            $imgLink = "<a href=\"" . base_url() . "index.php/Homepage/view/" . $profile['id'] . "\">" . $imgString . "</a>";
+        
+		$c= date('Y-M-D');
+		$y= date('Y-M-D', strtotime($profile['birthdate']));
+		$age = $c-$y -1;
+		
+		$descr = substr($profile['description'], 0, 250) . "...";
+		
+		echo "<div class=\"homepageProfileBlock\">
+		" . $imgLink . "
+		<h3>" . $profile['name'] . "</h3><br />
+		Nickname: " . $profile['nickname'] . "<br />
+		Age: " . $age . "<br />
+		Description: " . $descr . "<br />
+        Personality: " . $profile['personality'] . "
+        
+        
+		</div>";
 	}
 ?>
